197bbd84ac0e84d94043c4ed577894fc2aa2a807
gitconfig <dannankra@yahoo.com>
2016-07-01 16:56:15 -0400

    Added a photo token permission
    Now a terseinc user can make photo related requests

--- src/main/java/com/terseinc/graph/TokenManager.java
+++ src/main/java/com/terseinc/graph/TokenManager.java
@@ -4,8 +4,10 @@ package com.terseinc.graph;
  * Object to manage authentiations and facebook user tokens
  */
 public class TokenManager {
-	public static String get_about_me_Token(){
+	public static final String get_about_me_Token(){
 		return "EAAC13SkBFlgBANkewJcGZCXaRA6lOQrjihxEq3lRIdZCmNkvzbSG5KsOi5ZBYMBZA6uafgY3u6T6KgOMMO7dc4NUGdZBhpRMdQ2ZCbR6TbPZBBGKFuYeeKV3vduTZCYrdD6a2k8CKdY1Y4CXqcX21D6xJYZAOyoIlZB4WsOF0yAWYAIBku10PjNhEV";
 	}
-
+	public static final String get_photo_Token(){
+		return "EAAC13SkBFlgBAGzMIUFm5Go9li2dLzZAdkECwrUL8n2a2UwY5go1VzZAB3ugMPyZCJjwFrVlD8bX5NCeMtK9mHYV7IlbZARCvXlqNPZCCP66Dmk9USuJ44zRPtZBVosPIalwabybaUZBvCZC4HnkZA0ehrE0eHQrpAaS7FMANhdZBLvtYqYgDA7nRB";
+	}
 }
